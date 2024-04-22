57b49ca967627efad95d4456b7e57dc881fdc235
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2024-04-22 18:11:14 +0300

    on board initial commit

---
+++ on_board_screen/lib/feature/on_board_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoardView extends StatefulWidget {
+  const OnBoardView({super.key});
+
+  @override
+  State<OnBoardView> createState() => _OnBoardViewState();
+}
+
+class _OnBoardViewState extends State<OnBoardView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
