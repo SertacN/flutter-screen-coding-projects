7310f619262f466f85aad2e288eee0bd5b7ed7de
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2024-04-24 18:29:48 +0300

    delete trash object

--- lib/feature/sign_up_size.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final class SignUpSize {
-  SignUpSize._();
-
-  static final lowRadius = BorderRadius.circular(10);
-
-  static double signUpButtonSize = 54;
-}
